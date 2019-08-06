b5fb2c33c0672ae023bae5ed51c32af75851df9c
Минкин Вадим <vadim2806@gmail.com>
2019-08-06 21:31:44 +0300

    release last

---
+++ precache-manifest.ad856cde8d6ce317a35575099e45dec0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5589f2655936d48187ad",
+    "url": "/static/js/main.4e1fdccb.chunk.js"
+  },
+  {
+    "revision": "40697d7b239503758a0f",
+    "url": "/static/js/2.aebb2fd2.chunk.js"
+  },
+  {
+    "revision": "5589f2655936d48187ad",
+    "url": "/static/css/main.1c74c290.chunk.css"
+  },
+  {
+    "revision": "40697d7b239503758a0f",
+    "url": "/static/css/2.089e2b8e.chunk.css"
+  },
+  {
+    "revision": "147fd987d6737896102234814a70689b",
+    "url": "/index.html"
+  }
+];
